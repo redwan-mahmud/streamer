bea2c3e1413915ab955375cabff69cf23c3c8696
Redwan <redwan.mahmud07@gmail.com>
2020-12-08 00:27:05 -0800

    Added redux form, need to fix error display bug

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,62 @@
 import React from 'react'
+import {Field, getFormMeta, reduxForm} from 'redux-form'
 
-const StreamCreate = () => {
+ 
+class StreamCreate extends React.Component {
+    renderError({error,touched}){
+        if(touched && error){
+            return(
+                <div className = 'ui error message'>
+                    <div className = 'header'>{error}</div>
+                </div>
+            )
+        }
+    }
+    renderInput = ({input,label, meta}) => {
+        const className = `field ${meta.error && meta.touched ? 'error': ''}`
+        return(
+            <div className ={className}>
+                <label>{label}</label>
+                <input {...input} autoComplete = 'off'/>
+                {this.renderError(meta)}
+            </div>
+        )
+
+    }
+    onSubmit(formValues){
+        console.log(formValues)
+    }
+
+
+
+    render(){       
     return (
-        <div>
-            <h1>StreamCreate</h1>
-        </div>
+        <form onSubmit = {this.props.handleSubmit(this.onSubmit)} className = 'ui form error'>
+            <Field name = 'title' component = {this.renderInput} label = "Enter title"/>
+            <Field name = 'description' component = {this.renderInput} label = "Enter description"/>
+            <button className = 'ui button promary'>Submit</button>
+        </form>
+        
+
     )
 }
+}
 
-export default StreamCreate
+const validate = (formValues) => {
+    const errors = {} 
+
+    if(!formValues.title){
+        errors.title = 'Enter a title'
+    }
+    if(!formValues.description){
+        errors.title = 'Enter a description'
+    }
+
+    return errors
+}
+
+export default reduxForm({
+    form: 'streamCreate',
+    validate
+}
+)(StreamCreate)
