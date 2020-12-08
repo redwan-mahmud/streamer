28e5511e7cb6d819c7737dec737246f7603e4b21
redwan-mahmud <redwan.mahmud@icloud.com>
2020-12-08 15:53:29 -0800

    Added reducers, and created REST API

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Field, getFormMeta, reduxForm} from 'redux-form'
-
+import {Field, reduxForm} from 'redux-form'
+import {connect} from 'react-redux'
+import {createStream} from '../../actions'
  
 class StreamCreate extends React.Component {
     renderError({error,touched}){
@@ -23,17 +24,21 @@ class StreamCreate extends React.Component {
         )
 
     }
-    onSubmit(formValues){
-        console.log(formValues)
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues)
     }
 
 
 
     render(){       
     return (
-        <form onSubmit = {this.props.handleSubmit(this.onSubmit)} className = 'ui form error'>
+        <form onSubmit = {this.props.handleSubmit(this.onSubmit)} 
+        className = 'ui form error'>
+
             <Field name = 'title' component = {this.renderInput} label = "Enter title"/>
-            <Field name = 'description' component = {this.renderInput} label = "Enter description"/>
+            <Field name = 'description' 
+            component = {this.renderInput} 
+            label = "Enter description"/>
             <button className = 'ui button promary'>Submit</button>
         </form>
         
@@ -49,14 +54,16 @@ const validate = (formValues) => {
         errors.title = 'Enter a title'
     }
     if(!formValues.description){
-        errors.title = 'Enter a description'
+        errors.description = 'Enter a description'
     }
 
     return errors
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 }
 )(StreamCreate)
+
+export default connect(null, {createStream})(formWrapped)
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
+import reduxThunk from 'redux-thunk'
 import App from './components/App'
 import reducers from './reducers'
 
@@ -9,7 +10,7 @@ import reducers from './reducers'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   reducers, 
-  composeEnhancers(applyMiddleware())
+  composeEnhancers(applyMiddleware(reduxThunk))
 )
 
 ReactDOM.render(
@@ -18,8 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
