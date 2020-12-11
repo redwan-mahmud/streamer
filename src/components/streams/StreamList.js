2b538049fa4afc1dab24b18eabb7454910f12897
redwan-mahmud <redwan.mahmud@icloud.com>
2020-12-11 15:56:33 -0800

    Added some changes to modal to pass id

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -15,9 +15,9 @@ class StreamList extends React.Component {
                 <div className = "right floated content">
                     
                     <Link to={`/streams/edit/${stream.id}`} className ="ui button primary">Edit</Link>
-                    <button className = "ui button red">
+                    <Link to={`/streams/delete/${stream.id}`}className = "ui button red">
                         Delete
-                    </button>
+                    </Link>
                 </div>
             )
 
